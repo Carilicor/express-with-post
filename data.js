6e645e4bba7bc75cc76ecf227d5d8c551cfa57c1
Cari Licor <Carilicor@gmail.com>
2022-10-17 16:13:52 -0400

    practice with post

---
+++ data.js
@@ -0,0 +1,13 @@
+export default [{
+    "name": "Brad Jones",
+    "age": 30,
+    "birthday": "October 15,1992"
+},{
+    "name": "Todd Albert",
+    "age": 47,
+    "birthday": "October 17, 1975"
+},{ 
+    "name": "Cari Licor",
+    "age": 31,
+    "birthday": "November 7, 1990"
+}]
